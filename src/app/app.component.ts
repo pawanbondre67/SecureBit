80ad62af4a535eb60282a06025370425c20e593f
pawanbondre67 <bondrepavan623@gmail.com>
2024-04-15 05:18:43 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+
 })
-export class AppComponent {
-  title = 'SecureBit';
+export class AppComponent  {
+  
+  imagePath = '../assets/icons8-invisible-48.png';
+  
+  passwordArray:any[] = [];
+
+  list : any = {
+    URL : '',
+    username : '',
+    password : ''
+  }
+
+  constructor(){}
+
+  ngOnInit() : void{
+    const passwordList = localStorage.getItem("passwordList");
+    if(passwordList !== null){
+      this.passwordArray = JSON.parse(passwordList);
+    }
+  }
+
+copyText(text: string) {
+    alert('Copied to clipboard');
+    navigator.clipboard.writeText(text)
+    };
+  
+    deletePassword(index: number) {
+      // Remove the item from the array
+      this.passwordArray.splice(index, 1);
+  
+      // Update the localStorage
+      localStorage.setItem("passwordList", JSON.stringify(this.passwordArray));
+  }
+    
+
+  showpassword() : void{
+    let x = document.getElementById("password") as HTMLInputElement;
+   
+    if(x !== null && x.type === "password"){
+      x.type = "text";
+     this.imagePath = '../assets/icons8-eye-24.png';
+      
+    }else{
+      x.type = "password";
+      this.imagePath = '../assets/icons8-invisible-48.png';
+    }
+
+  }
+
+   savepassword(data:any){
+
+      //  debugger;
+      this.passwordArray.push(this.list);
+
+      console.log(this.passwordArray);
+
+      localStorage.setItem("passwordList", JSON.stringify(this.passwordArray));
+      this.list = {
+        URL : '',
+        username : '',
+        password : ''
+      }
+           
+
+   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+  
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+    
   ],
   providers: [
     provideClientHydration()
